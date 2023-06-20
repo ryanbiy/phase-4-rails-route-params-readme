55dbdaf94f448c28ee23f93ff2deaac4374dc891
Ryan <ryanbiy01@gmail.com>
2023-06-20 14:37:05 +0300

    completed assignment

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -4,5 +4,10 @@ class CheesesController < ApplicationController
     cheeses = Cheese.all
     render json: cheeses
   end
+  def show
+    
+    cheese=Cheese.find_by(params[:id])
+    render json: cheese
+  end 
 
 end
